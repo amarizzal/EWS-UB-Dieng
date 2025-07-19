3f8aedb3d935b72d39cf5b9b2f0a8486d1171b4f
rizal <rizal.palms@gmail.com>
2025-07-19 09:13:43 +0700

    LOGIN - add login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'showLoginForm']);
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
